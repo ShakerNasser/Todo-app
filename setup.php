df766111b56e6e2801341c9c0b2f7590ad962552
ShakerNasser <shaker.nasser@chasacademy.se>
2023-12-19 22:55:29 +0000

    Börjar med U04

---
+++ setup.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "db"; //Samma namn som servicen i docker-compose.yml (vissa exempel har "localhost" men då måste man specifiera porten)
+$username = "mariadb";
+$password = "mariadb";
+$dbname = "mariadb";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br>";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
